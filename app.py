ca19e242c894af74595fe2ed9a1c60491756b2ab
Viktor Viskov <vivi.skp@edu.mercantec.dk>
2022-06-22 12:50:17 +0200

    Created first ver of fuel parser

---
+++ app.py
@@ -0,0 +1,16 @@
+# stations
+import subprocess
+from ok import OK_STATION
+from q8 import Q8_STATION
+from f24 import F24_STATION
+from circle import CIRCLE_STATION
+from ingo import INGO_STATION
+from goon import GOON_STATION
+
+# load prices
+# print(OK_STATION().get_prices())
+# print(Q8_STATION().get_prices())
+# print(F24_STATION().get_prices())
+# print(CIRCLE_STATION().get_prices())
+# print(INGO_STATION().get_prices())
+# print(GOON_STATION().get_prices())
